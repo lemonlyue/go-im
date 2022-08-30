0eb119fb553b0d19af92239fec8d11d19cf4ce0a
lemon_lyue <lemon_lyue@163.com>
2022-08-30 14:28:45 +0800

    router

--- router/router.go
+++ router/router.go
@@ -2,11 +2,21 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func Router(app *gin.Engine)  {
+func Router(app *gin.Engine) {
+	// 404
+	app.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"code": 404,
+			"message": "Not Found",
+		})
+	})
+	// 测试路由
 	app.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
+			"code": 200,
 			"message": "pong",
 		})
 	})
